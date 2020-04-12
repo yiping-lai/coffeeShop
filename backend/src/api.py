515d9991b0b7e48ca9382f9fb168d3f996e488e8
yiping-lai <yipinglai627@gmail.com>
2020-04-12 13:42:22 -0700

    second submission

--- backend/src/api.py
+++ backend/src/api.py
@@ -16,7 +16,7 @@ CORS(app)
 uncomment the following line to initialize the datbase
 
 '''
-#db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
 '''
@@ -24,7 +24,8 @@ implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks} 
+    where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks')
@@ -42,7 +43,8 @@ implement endpoint
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
+    returns status code 200 and json {"success": True, "drinks": drinks}
+    where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks-detail')
@@ -62,7 +64,8 @@ implement endpoint
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
+    returns status code 200 and json {"success": True, "drinks": drink}
+     where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks', methods=["POST"])
@@ -95,7 +98,8 @@ implement endpoint
         it should update the corresponding row for <id>
         it should require the 'patch:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+    returns status code 200 and json {"success": True, "drinks": drink} 
+    where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<int:id>', methods=["PATCH"])
@@ -130,7 +134,8 @@ implement endpoint
         it should respond with a 404 error if <id> is not found
         it should delete the corresponding row for <id>
         it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+    returns status code 200 and json {"success": True, "delete": id} 
+    where id is the id of the deleted record
         or appropriate status code indicating reason for failure
 '''
 @app.route('/drinks/<int:id>', methods=["DELETE"])
